8417b8b7ffe5386e104491eeaeb86ba8d78b3bbf
Zahra Azin <ztara.azin@gmail.com>
2020-07-27 16:28:29 -0400

    update js file

--- script.js
+++ script.js
@@ -1,238 +1,106 @@
-// Select everything
-const dateElement = document.getElementById('date');
-const saveElements = document.getElementsByClassName('save');
-const contentElements = document.getElementsByClassName('content');
-// select all content fields
-const nineAM = document.getElementById('9am');
-const tenAM = document.getElementById('10am');
-const elevenAM = document.getElementById('11am');
-const twelvePM = document.getElementById('12pm');
-const onePM = document.getElementById('1pm');
-const twoPM = document.getElementById('2pm');
-const threePM = document.getElementById('3pm');
-const fourPM = document.getElementById('4pm');
-const fivePM = document.getElementById('5pm');
+// Moment.js
+var currentDate = moment().format('dddd') + " " + moment().format("Do MMM YYYY");
+var currentHour = moment().format('h:mm:ss a');
+// Text hour var
+var nineAm = $("#9am");
+var tenAm = $("#10am");
+var elevenAm = $("#11am");
+var twelvePm = $("#12pm");
+var onePm = $("#13pm");
+var twoPm = $("#14pm");
+var threePm = $("#15pm");
+var fourPm = $("#16pm");
+var fivePm = $("#17pm");
+var sixPm = $("#18pm");
+var sevenPm = $("#19pm");
 
-const saveNine = document.getElementById('saveNine');
-const saveNineField = () => {
-  let nineAMText = nineAM.value;
-  localStorage.setItem("nine", nineAMText);
-  console.log(localStorage.getItem("nine"));
-}
-saveNine.addEventListener("click", saveNineField);
+var hour = moment().hours();
+var userInput;
+var hourSpan;
+// var hourString = $(".hour").text().split(" ");
 
-const saveTen = document.getElementById('saveTen');
-const saveTenField = () => {
-  let tenAMText = tenAM.value;
-  localStorage.setItem("ten", tenAMText);
-  console.log(localStorage.getItem("ten"));
-}
-saveTen.addEventListener("click", saveTenField);
+// Date and Hour
 
-const saveEleven = document.getElementById('saveEleven');
-const saveElevenField = () => {
-  let elevenAMText = elevenAM.value;
-  localStorage.setItem("eleven", elevenAMText);
-  console.log(localStorage.getItem("eleven"));
-}
-saveEleven.addEventListener("click", saveElevenField);
+var interval = setInterval(function() {
+  var momentNow = moment();
+  $('#currentDay').html(momentNow.format('YYYY MMMM DD') + ' '
+                      + momentNow.format('dddd')
+                       .substring(0,3).toUpperCase());
+  $('#currentDay').html(currentDate + " " + momentNow.format('hh:mm:ss A'));
+}, 100);
 
-const saveTwelve = document.getElementById('saveTwelve');
-const saveTwelveField = () => {
-  let twelvePMText = twelvePM.value;
-  localStorage.setItem("twelve", twelvePMText);
-}
-saveTwelve.addEventListener("click", saveTwelveField);
+function initPage() {
 
-const saveOne = document.getElementById('saveOne');
-const saveOneField = () => {
-  let onePMText = onePM.value;
-  localStorage.setItem("one", onePMText);
-  console.log(localStorage.getItem("one"));
-}
-saveOne.addEventListener("click", saveOneField);
+  console.log("Current Hour " + hour);
+  var init9 = JSON.parse(localStorage.getItem("09:00 am"));
+  nineAm.val(init9);
 
-const saveTwo = document.getElementById('saveTwo');
-const saveTwoField = () => {
-  let twoPMText = twoPM.value;
-  localStorage.setItem("two", twoPMText);
-  console.log(localStorage.getItem("two"));
-}
-saveTwo.addEventListener("click", saveTwoField);
+  var init10 = JSON.parse(localStorage.getItem("10:00 am"))
+  tenAm.val(init10);
+  
+  var init11 = JSON.parse(localStorage.getItem("11:00 am"))
+  elevenAm.val(init11);
+  
+  var init12 = JSON.parse(localStorage.getItem("12:00 pm"))
+  twelvePm.val(init12);
+  
+  var init1 = JSON.parse(localStorage.getItem("01:00 pm"))
+  onePm.val(init1);
+  
+  var init2 = JSON.parse(localStorage.getItem("02:00 pm"))
+  twoPm.val(init2);
+  
+  var init3 = JSON.parse(localStorage.getItem("03:00 pm"))
+  threePm.val(init3);
+ 
+  var init4 = JSON.parse(localStorage.getItem("04:00 pm"))
+  fourPm.val(init4);
+  
+  var init5 = JSON.parse(localStorage.getItem("05:00 pm"))
+  fivePm.val(init5);
+  
+  var init6 = JSON.parse(localStorage.getItem("06:00 pm"))
+  sixPm.val(init6);
+  
+  var init7 = JSON.parse(localStorage.getItem("07:00 pm"))
+  sevenPm.val(init7);
+} 
 
-const saveThree = document.getElementById('saveThree');
-const saveThreeField = () => {
-  let threePMText = threePM.value;
-  localStorage.setItem("three", threePMText);
-  console.log(localStorage.getItem("three"));
-}
-saveThree.addEventListener("click", saveThreeField);
-
-const saveFour = document.getElementById('saveFour');
-const saveFourField = () => {
-  let fourPMText = fourPM.value;
-  localStorage.setItem("four", fourPMText);
-  console.log(localStorage.getItem("four"));
-}
-saveFour.addEventListener("click", saveFourField);
-
-const saveFive = document.getElementById('saveFive');
-const saveFiveField = () => {
-  let fivePMText = fivePM.value;
-  localStorage.setItem("five", fivePMText);
-  console.log(localStorage.getItem("five"));
-}
-saveFive.addEventListener("click", saveFiveField);
-
-
-let toDoList = [];
-
-const initPlanner = () => {
-    updateContent();
-    getDateAndTime();
-    checkTimes();
+function background () {
+      
+  $(".form-control").each(function () {
+      var timeTest = parseInt($(this).attr("id"));
+      hour = parseInt(hour);
+      console.log(timeTest);
+      console.log(hour);
+//      console.log(this);
+      if (hour > timeTest) {
+          $(this).addClass("past");
+      } else if (hour < timeTest) {
+          $(this).addClass("future");
+      } else {
+          $(this).addClass("present");
+      }
+  });
 }
 
-// function to display date and time at top of page
-const getDateAndTime = () => {
-  let dateAndTime = moment().format("dddd, MMMM Do YYYY, h:mm a");
-  dateElement.innerHTML = dateAndTime;
-}
+$(document).ready(function(){
+  initPage()
+  background()
 
-const updateContent = () => {
-  nineAM.innerHTML = localStorage.getItem("nine");
-  tenAM.innerHTML = localStorage.getItem("ten");
-  elevenAM.innerHTML = localStorage.getItem("eleven");
-  twelvePM.innerHTML = localStorage.getItem("twelve");
-  onePM.innerHTML = localStorage.getItem("one");
-  twoPM.innerHTML = localStorage.getItem("two");
-  threePM.innerHTML = localStorage.getItem("three");
-  fourPM.innerHTML = localStorage.getItem("four");
-  fivePM.innerHTML = localStorage.getItem("five");
-}
+  // Buttons (save to Local Storage)
+  $(".saveBtn").on("click", function(){
+    userInput = $(this).siblings(".form-control").val().trim();
+    console.log(userInput);
+    hourSpan = $(this).siblings(".input-group-prepend").text().trim();
+    console.log(hourSpan);
+    localStorage.setItem(hourSpan, JSON.stringify(userInput));
 
-const checkTimes = () => {
-  let currentTime = moment().hour();
-  if (currentTime >= 9 && currentTime < 10) {
-    nineAM.style.background = "#fa6c14";
-  }
-  else if (currentTime >= 10 && currentTime < 11) {
-    nineAM.style.background = "lightgrey";
-    nineAM.setAttribute("readonly", true);
-    tenAM.style.background = "#fa6c14";
-  }
-  else if (currentTime >= 11 && currentTime < 12) {
-    nineAM.style.background = "lightgrey";
-    nineAM.setAttribute("readonly", true);
-    tenAM.style.background = "lightgrey";
-    tenAM.setAttribute("readonly", true);
-    elevenAM.style.background = "#fa6c14";
-  }
-  else if (currentTime >= 12 && currentTime < 13) {
-    nineAM.style.background = "lightgrey";
-    nineAM.setAttribute("readonly", true);
-    tenAM.style.background = "lightgrey";
-    tenAM.setAttribute("readonly", true);
-    elevenAM.style.background = "lightgrey";
-    elevenAM.setAttribute("readonly", true);
-    twelvePM.style.background = "#fa6c14";
-  }
-  else if (currentTime >= 13 && currentTime < 14) {
-    nineAM.style.background = "lightgrey";
-    nineAM.setAttribute("readonly", true);
-    tenAM.style.background = "lightgrey";
-    tenAM.setAttribute("readonly", true);
-    elevenAM.style.background = "lightgrey";
-    elevenAM.setAttribute("readonly", true);
-    twelvePM.style.background = "lightgrey";
-    twelvePM.setAttribute("readonly", true);
-    onePM.style.background = "#fa6c14";
-  }
-  else if (currentTime >= 14 && currentTime < 15) {
-    nineAM.style.background = "lightgrey";
-    nineAM.setAttribute("readonly", true);
-    tenAM.style.background = "lightgrey";
-    tenAM.setAttribute("readonly", true);
-    elevenAM.style.background = "lightgrey";
-    elevenAM.setAttribute("readonly", true);
-    twelvePM.style.background = "lightgrey";
-    twelvePM.setAttribute("readonly", true);
-    onePM.style.background = "lightgrey";
-    onePM.setAttribute("readonly", true);
-    twoPM.style.background = "#fa6c14";
-  }
-  else if (currentTime >= 15 && currentTime < 16) {
-    nineAM.style.background = "lightgrey";
-    nineAM.setAttribute("readonly", true);
-    tenAM.style.background = "lightgrey";
-    tenAM.setAttribute("readonly", true);
-    elevenAM.style.background = "lightgrey";
-    elevenAM.setAttribute("readonly", true);
-    twelvePM.style.background = "lightgrey";
-    twelvePM.setAttribute("readonly", true);
-    onePM.style.background = "lightgrey";
-    onePM.setAttribute("readonly", true);
-    twoPM.style.background = "lightgrey";
-    twoPM.setAttribute("readonly", true);
-    threePM.style.background = "#fa6c14";
-  }
-  else if (currentTime >= 16 && currentTime < 17) {
-    nineAM.style.background = "lightgrey";
-    nineAM.setAttribute("readonly", true);
-    tenAM.style.background = "lightgrey";
-    tenAM.setAttribute("readonly", true);
-    elevenAM.style.background = "lightgrey";
-    elevenAM.setAttribute("readonly", true);
-    twelvePM.style.background = "lightgrey";
-    twelvePM.setAttribute("readonly", true);
-    onePM.style.background = "lightgrey";
-    onePM.setAttribute("readonly", true);
-    twoPM.style.background = "lightgrey";
-    twoPM.setAttribute("readonly", true);
-    threePM.style.background = "lightgrey";
-    threePM.setAttribute("readonly", true);
-    fourPM.style.background = "#fa6c14";
-  }
-  else if (currentTime >= 17 && currentTime < 18) {
-    nineAM.style.background = "lightgrey";
-    nineAM.setAttribute("readonly", true);
-    tenAM.style.background = "lightgrey";
-    tenAM.setAttribute("readonly", true);
-    elevenAM.style.background = "lightgrey";
-    elevenAM.setAttribute("readonly", true);
-    twelvePM.style.background = "lightgrey";
-    twelvePM.setAttribute("readonly", true);
-    onePM.style.background = "lightgrey";
-    onePM.setAttribute("readonly", true);
-    twoPM.style.background = "lightgrey";
-    twoPM.setAttribute("readonly", true);
-    threePM.style.background = "lightgrey";
-    threePM.setAttribute("readonly", true);
-    fourPM.style.background = "lightgrey";
-    fourPM.setAttribute("readonly", true);
-    fivePM.style.background = "#fa6c14";
-  }
-  else if (currentTime >= 18) {
-    nineAM.style.background = "lightgrey";
-    nineAM.setAttribute("readonly", true);
-    tenAM.style.background = "lightgrey";
-    tenAM.setAttribute("readonly", true);
-    elevenAM.style.background = "lightgrey";
-    elevenAM.setAttribute("readonly", true);
-    twelvePM.style.background = "lightgrey";
-    twelvePM.setAttribute("readonly", true);
-    onePM.style.background = "lightgrey";
-    onePM.setAttribute("readonly", true);
-    twoPM.style.background = "lightgrey";
-    twoPM.setAttribute("readonly", true);
-    threePM.style.background = "lightgrey";
-    threePM.setAttribute("readonly", true);
-    fourPM.style.background = "lightgrey";
-    fourPM.setAttribute("readonly", true);
-    fivePM.style.background = "lightgrey";
-    fivePM.setAttribute("readonly", true);
-  }
+  })
+  // Button for clear the day
+  $("#clearDay").on("click", function(){
+    localStorage.clear();
+    initPage()
+  }) 
 
-}
-
-initPlanner();
+});
